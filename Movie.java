d87949fb8daa40b93ee03f394947115eb3ebfbea
emalady1 <22328180+emalady1@users.noreply.github.com>
2020-09-28 00:28:57 -0500

    movie.java with generics and getters and setters

--- Movie.java
+++ Movie.java
@@ -1,32 +1,65 @@
 
-public class Movie {
+public class Movie<KeyType, ValueType> {
 
-	String name;
-	String eidr;
-	String genre;
+	private KeyType name;
+	private ValueType eidr;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * creates an object of type movie
+	 * 
+	 * @param givenName  user inputted name of movie
+	 * @param givenEidr  user inputted EIDR identification number
+	 * @param givenGenre user inputted genre
+	 */
+	public Movie(KeyType givenName, ValueType givenEidr) {
+		name = givenName;
+		eidr = givenEidr;
 
 	}
 
 	/**
-	 * creates an object of type movie
-	 * @param givenName user inputted name of movie
-	 * @param givenEidr user inputted EIDR identification number
-	 * @param givenGenre user inputted genre
+	 * returns name
+	 * 
+	 * @return name of movie object
 	 */
-	public Movie(String givenName, String givenEidr, String givenGenre) {
-		name = givenName;
-		eidr = givenEidr;
-		genre = givenGenre;
+	public KeyType getName() {
+		return name;
+
+	}
+
+	/**
+	 * returns eidr
+	 * 
+	 * @return eidr code of movie object
+	 */
+	public ValueType getEidr() {
+		return eidr;
+
+	}
+
+	/**
+	 * sets name of movie object with new name
+	 * 
+	 * @param newName name to give to movie object
+	 */
+	public void setName(KeyType newName) {
+		name = newName;
+	}
+
+	/**
+	 * sets EIDR of movie object with new Eidr code
+	 * 
+	 * @param newEidr code to give to movie object
+	 */
+	public void setEidr(ValueType newEidr) {
+		eidr = newEidr;
 	}
 
 	/**
 	 * @return string with name of the movie object
 	 */
 	public String toString() {
-		return (name);
+		return (String) (name);
 	}
 
 }
