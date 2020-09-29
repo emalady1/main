5de1b0ea4beb7876d1d11564cb9837047e3d6546
JackWubbolding <71193750+JackWubbolding@users.noreply.github.com>
2020-09-28 23:03:49 -0500

    Update IMDB.java

--- IMDB.java
+++ IMDB.java
@@ -1,43 +1,47 @@
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class loads a list of movies with their corresponding EDIR number to an array.
+ * @author jack
+ *
+ */
 public class IMDB {
+  static ArrayList<Movie> movieList;
 
-	static ArrayList<Movie> movieList;
+  public IMDB() {
 
-	public static void main(String[] args) {
+    movieList = new ArrayList<Movie>();
+    try {
+      loadMovies();
 
-		movieList = new ArrayList<Movie>();
-		try {
-			loadMovies();
+    } catch (IOException e) {
+      System.out.println("error caught");
 
-		} catch (IOException e) {
-			System.out.println("error caught");
+    }
+    System.out.println(movieList.toString());
+  }
 
-		}
-		System.out.println(movieList.toString());
-	}
+  /**
+   * loads the movie files and adds them to an array of movie objects
+   * @throws IOException if file is not in correct format
+   */
+  public static void loadMovies() throws IOException {
+    File movies = new File("/Users/jack/eclipse-workspace/Project One "
+        + "Movie Database/src/Movies.txt"); 
 
-	/**
-	 * loads the movie files and adds them to an array of movie objects
-	 * @throws IOException if file is not in correct format
-	 */
-	public static void loadMovies() throws IOException {
-		File movies = new File("/Users/elliemalady/Desktop/Movies.txt");
+    Scanner scanner = new Scanner(movies);
 
-		Scanner scanner = new Scanner(movies);
+    while (scanner.hasNextLine()) {
+      String[] split = scanner.nextLine().split(" ");
 
-		while (scanner.hasNextLine()) {
-			String[] split = scanner.nextLine().split(" ");
+      Movie curr = new Movie(split[0], split[1]);
 
-			Movie curr = new Movie(split[0], split[1]);
+      movieList.add(curr);
 
-			movieList.add(curr);
-
-		}
-	}
+    }
+  }
 
 }
